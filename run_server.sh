2ed0ddd6e0072d86e939efaa18f148bfb0590aa7
Jason Benterou <jason.benterou+github@gmail.com>
2015-06-21 13:57:05 -0700

    Update script

--- run_server.sh
+++ run_server.sh
@@ -1,7 +1,9 @@
+docker rm playingcard0
+
 docker run \
     --name playingcard0 \
     -d \
-    -p 8888:8888 \
+    -p 8889:8888 \
     -v /Users/jason/src/play/playingcard-collision/notebooks:/notebooks \
     -e "PASSWORD=mugwumpjismsyn2" \
     jtwb/playingcard-ipynb
